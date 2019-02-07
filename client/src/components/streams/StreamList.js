ac368c658ab7dcfb30b2e5d1384c7ac7f204df7e
soulmecca <soulmecca@gmail.com>
2019-02-07 16:17:31 -0500

    added renderCreate function to put create button when logged in

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchStreams } from "../../actions";
+import { Link } from "react-router-dom";
 
 class StreamList extends React.Component {
 	componentDidMount() {
@@ -18,7 +19,17 @@ class StreamList extends React.Component {
 		}
 	}
 
-	renderCreate() {}
+	renderCreate() {
+		if (this.props.isSignedIn) {
+			return (
+				<div style={{ textAlign: "right" }}>
+					<Link to="/streams/new" className="ui button primary">
+						Create Stream
+					</Link>
+				</div>
+			);
+		}
+	}
 
 	renderList() {
 		return this.props.streams.map(stream => {
@@ -40,13 +51,18 @@ class StreamList extends React.Component {
 			<div>
 				<h2>Streams</h2>
 				<div className="ui celled list">{this.renderList()}</div>
+				{this.renderCreate()}
 			</div>
 		);
 	}
 }
 
 const mapStateToProps = state => {
-	return { streams: Object.values(state.streams), currentUid: state.auth.uid };
+	return {
+		streams: Object.values(state.streams),
+		currentUid: state.auth.uid,
+		isSignedIn: state.auth.isSignedIn
+	};
 };
 
 export default connect(
