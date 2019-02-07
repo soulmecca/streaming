fceb7d7f1a526eebc073efd4ef2f81517a6eba4e
soulmecca <soulmecca@gmail.com>
2019-02-06 22:29:57 -0500

    added types.js for action types variables

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -61,7 +61,7 @@ class GoogleAuth extends React.Component {
 }
 
 const mapStateToProps = state => {
-	return { isSignedIn: state.auth.isSignedIN };
+	return { isSignedIn: state.auth.isSignedIn };
 };
 
 export default connect(
