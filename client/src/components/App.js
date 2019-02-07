c0f29b9cf2701583c8f9f5e09aa84ea92a3c2507
soulmecca <soulmecca@gmail.com>
2019-02-07 11:54:55 -0500

    added a form for StreamCreate with redux-form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -14,10 +14,10 @@ const App = () => {
 				<div>
 					<Header />
 					<Route path="/" exact component={StreamList} />
-					<Route path="/streams/new" exact component={StreamCreate} />
-					<Route path="/streams/edit" exact component={StreamEdit} />
-					<Route path="/streams/delete" exact component={StreamDelete} />
-					<Route path="/streams/show" exact component={StreamShow} />
+					<Route path="/streams/new" exact render={StreamCreate.render} />
+					<Route path="/streams/edit" component={StreamEdit} />
+					<Route path="/streams/delete" component={StreamDelete} />
+					<Route path="/streams/show" component={StreamShow} />
 				</div>
 			</BrowserRouter>
 		</div>
