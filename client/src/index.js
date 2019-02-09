a2261858a8236677fdafde9b733514e95d3a77e7
soulmecca <soulmecca@gmail.com>
2019-02-09 09:18:45 -0500

    hook up connected-react-router to store

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import reduxThunk from "redux-thunk";
+import { ConnectedRouter, routerMiddleware } from "connected-react-router";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
-import reduxThunk from "redux-thunk";
-
+import history from "./history";
 import App from "./components/App";
 import reducers from "./reducers";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middlewares = [reduxThunk, routerMiddleware(history)];
 const store = createStore(
 	reducers,
-	composeEnhancers(applyMiddleware(reduxThunk))
+	composeEnhancers(applyMiddleware(...middlewares))
 );
 
 ReactDOM.render(
 	<Provider store={store}>
-		<App />
+		<ConnectedRouter history={history}>
+			<App />
+		</ConnectedRouter>
 	</Provider>,
 	document.querySelector("#root")
 );
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,12 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import { reducer as formReducer } from "redux-form";
+import { connectRouter } from "connected-react-router";
 import streamReducer from "./streamReducer";
+import history from "../history";
 
 export default combineReducers({
+	router: connectRouter(history),
 	auth: authReducer,
 	form: formReducer,
 	streams: streamReducer
